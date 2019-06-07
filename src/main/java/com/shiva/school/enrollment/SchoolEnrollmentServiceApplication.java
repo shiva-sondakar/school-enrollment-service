45341db9305d08877d49ede251b7acc405f88cbc
SHIVANAND SONDAKAR <shiva@SHIVANANDs-MacBook-Air.local>
2019-06-08 01:19:31 +0530

    initial commit

---
+++ src/main/java/com/shiva/school/enrollment/SchoolEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.school.enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolEnrollmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolEnrollmentServiceApplication.class, args);
+	}
+
+}
